ae718886147e97997779c98a2aadfdfd730e7c09
Priscilla Handoyo <mariapriscilladeanne.handoyo@students.mq.edu.au>
2025-12-22 08:10:34 +1100

    221225_01 - Add Built-In components: MiniMap and Controls

--- easydraw/src/App.svelte
+++ easydraw/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
-  import { SvelteFlow, Background } from "@xyflow/svelte";
+  import { SvelteFlow, Background, Controls, MiniMap } from "@xyflow/svelte";
   import "@xyflow/svelte/dist/style.css";
 </script>
 
 <div style="width: 100vw; height: 100vh;">
   <SvelteFlow fitView>
     <Background />
+    <Controls />
+    <MiniMap />
   </SvelteFlow>
 </div>
