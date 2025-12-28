4fca434c9cc747b6a089e9b4b12ea87c6c6bf00a
Priscilla Handoyo <mariapriscilladeanne.handoyo@students.mq.edu.au>
2025-12-29 09:31:26 +1100

    291225_01 - Adding Nodes

--- easydraw/src/App.svelte
+++ easydraw/src/App.svelte
@@ -1,10 +1,23 @@
 <script>
   import { SvelteFlow, Background, Controls, MiniMap } from "@xyflow/svelte";
   import "@xyflow/svelte/dist/style.css";
+
+  let nodes = $state.raw([
+    {
+      id: "1",
+      position: { x: 0, y: 0 },
+      data: { label: "Hello" },
+    },
+    {
+      id: "2",
+      position: { x: 100, y: 100 },
+      data: { label: "World" },
+    },
+  ]);
 </script>
 
 <div style="width: 100vw; height: 100vh;">
-  <SvelteFlow fitView>
+  <SvelteFlow fitView bind:nodes>
     <Background />
     <Controls />
     <MiniMap />
