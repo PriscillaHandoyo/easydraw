2f586b35dc205848a88a8fef32e823da523e2486
Priscilla Handoyo <mariapriscilladeanne.handoyo@students.mq.edu.au>
2025-12-29 09:43:11 +1100

    291225_03 - Add Built-In components: node and edge types

--- easydraw/src/App.svelte
+++ easydraw/src/App.svelte
@@ -5,17 +5,27 @@
   let nodes = $state.raw([
     {
       id: "1",
+      type: "input",
       position: { x: 0, y: 0 },
       data: { label: "Hello" },
     },
     {
       id: "2",
+      type: "output",
       position: { x: 100, y: 100 },
       data: { label: "World" },
     },
   ]);
 
-  let edges = $state.raw([{ id: "e1-2", source: "1", target: "2" }]);
+  let edges = $state.raw([
+    {
+      id: "e1-2",
+      source: "1",
+      target: "2",
+      type: "smoothstep",
+      label: "to the",
+    },
+  ]);
 </script>
 
 <div style="width: 100vw; height: 100vh;">
