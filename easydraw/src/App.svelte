9941b3ae7bbd81ba7589bc3d1d6c2916b93fbab0
Priscilla Handoyo <mariapriscilladeanne.handoyo@students.mq.edu.au>
2025-12-29 10:19:34 +1100

    291225_04 - Add shape logic: custom nodes, custom edges, and magic button to add shapes

--- easydraw/src/App.svelte
+++ easydraw/src/App.svelte
@@ -1,37 +1,49 @@
 <script>
-  import { SvelteFlow, Background, Controls, MiniMap } from "@xyflow/svelte";
+  import {
+    SvelteFlow,
+    Background,
+    Controls,
+    MiniMap,
+    addEdge,
+  } from "@xyflow/svelte";
   import "@xyflow/svelte/dist/style.css";
 
-  let nodes = $state.raw([
-    {
-      id: "1",
-      type: "input",
-      position: { x: 0, y: 0 },
-      data: { label: "Hello" },
-    },
-    {
-      id: "2",
-      type: "output",
-      position: { x: 100, y: 100 },
-      data: { label: "World" },
-    },
-  ]);
+  let nodes = $state.raw([]);
 
-  let edges = $state.raw([
-    {
-      id: "e1-2",
-      source: "1",
-      target: "2",
-      type: "smoothstep",
-      label: "to the",
-    },
-  ]);
+  function addNode() {
+    const id = `${nodes.length + 1}`;
+
+    const newNode = {
+      id,
+      type: "default",
+      position: { x: Math.random() * 400, y: Math.random() * 400 },
+      data: { label: `Node ${id}` },
+    };
+
+    nodes = [...nodes, newNode];
+    $: console.log(nodes);
+  }
+
+  let edges = $state.raw([]);
+
+  function onConnect(params) {
+    edges = addEdge(params, edges);
+  }
 </script>
 
 <div style="width: 100vw; height: 100vh;">
-  <SvelteFlow fitView bind:nodes bind:edges>
+  <SvelteFlow fitView {nodes} {edges} onconnect={onConnect}>
     <Background />
     <Controls />
     <MiniMap />
   </SvelteFlow>
+</div>
+
+<div
+  class="controls"
+  style="position: absolute; z-index: 10; top: 10px; left: 10px;"
+>
+  <button onclick={addNode} style="padding: 10px; cursor: pointer;">
+    Add New Node
+  </button>
 </div>
